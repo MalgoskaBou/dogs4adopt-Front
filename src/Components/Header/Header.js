0db44cb8e287e8ef16a90ce08b9c2b3a442a15aa
MalgoskaG <ghouga.m@gmail.com>
2019-06-01 22:47:14 +0200

    formatowanie plikow z prettierem

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,6 @@
-import React from 'react'
-import styles from './Header.module.scss'
+import React from 'react';
+import styles from './Header.module.scss';
 
-const Header = () =>(
-    <h1 className = {styles.H1}>dogs 4 adopt</h1>
-)
+const Header = () => <h1 className={styles.H1}>dogs 4 adopt</h1>;
 
-export default Header
+export default Header;
