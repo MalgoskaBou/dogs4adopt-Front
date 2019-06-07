da421e3022f85464140ce32ceacf7ef7cebb633b
jurkotroll <jurko.bossetroll@gmail.com>
2019-06-07 15:12:50 +0200

    simple router sed down

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-expressions */
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 import './styles.css';
 
-const App = () => <Header />;
+const MainPage = () => {
+  return <h1>MainPage</h1>;
+};
+
+const SheltersPage = () => {
+  return <h1>SheltersPage</h1>;
+};
+
+const DogsPage = () => {
+  return <h1>DogsPage</h1>;
+};
+
+const AboutPage = () => {
+  return <h1>AboutPage</h1>;
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/dogs" component={DogsPage} />
+        <Route path="/shelters" component={SheltersPage} />
+        <Route path="/about" component={AboutPage} />
+      </Switch>
+
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
-const Header = () => <h1 className={styles.H1}>dogs 4 adopt</h1>;
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.header__logo}>dogs 4 adopt</h1>
+      <nav>
+        <ul className={styles.nav__menu}>
+          <li className={styles.nav__element}>
+            <Link to="/">Strona Główna</Link>
+          </li>
+          <li className={styles.nav__element}>
+            <Link to="/dogs">Psy</Link>
+          </li>
+          <li className={styles.nav__element}>
+            <Link to="/shelters">Schroniska</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
