7975399ee8d2f64ab556ef3a2612b5a787aa237f
jurisportfolio <jurko.bossetroll@gmail.com>
2019-06-15 12:38:52 +0200

    proposition: keep styles in [component].style.js

--- src/Components/HeaderComponent/HeaderComponent.js
+++ src/Components/HeaderComponent/HeaderComponent.js
@@ -1,38 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import headerBgImg from '../../assets/img/bonesPattern.png';
-import colors from '../../Styles/colors';
-
-const Li = styled.li`
-  display: block;
-  margin: 10px;
-  padding: 5px;
-  border-radius: 5px;
-  border: 1px solid green;
-  cursor: pointer;
-`;
-
-const Ul = styled.ul`
-  display: flex;
-  flex-direction: row;
-`;
-
-const LogoText = styled.h1``;
-
-const Header = styled.header`
-  background-image: url(${headerBgImg});
-  background-color: ${colors.primaryColor};
-  padding: 20px;
-  font-size: 20px;
-  color: ${colors.lightTextColor};
-  text-transform: uppercase;
-  margin-bottom: 50px;
-  font-family: 'Ostrich';
-  letter-spacing: 6px;
-  display: flex;
-  justify-content: space-between;
-`;
+import { Header, Ul, Li, LogoText } from './HeaderComponent.style';
 
 const HeaderComponent = () => {
   return (
